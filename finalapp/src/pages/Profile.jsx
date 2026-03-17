5ff52f0c8253d4608f286a75ca561c8fce2655c0
“erik1D” <eraik.diaz7830@gmail.com>
2026-03-16 23:21:20 -0700

    Added more User Profile content

--- finalapp/src/pages/Profile.jsx
+++ finalapp/src/pages/Profile.jsx
@@ -1,14 +1,22 @@
 import Heading from "../components/Heading";
 import AbtUsPics from "../components/AboutUsPics";
 import Bio from "../components/BioTextBox";
+import CommunitiesJoined from "../components/CommunitiesJoined";
 
 function Profile() {
   return(
     <div>
       <Heading myTitle="PROFILE PAGE"/>
       <div className="flex justify-center">
-      <AbtUsPics/>
-      <Bio/>
+        <AbtUsPics/>
+        <Bio/>
+      </div>
+      <div className="flex justify-center mt-50">
+        <CommunitiesJoined com1="Office workers" com2="Women's Group" com3="College Students"/>
+      </div>
+
+      <div className="flex justify-center mt-50">
+        <CommunitiesJoined com1="Liked a post about welcome circle" com2="Joined Women's Group" com3="Liked a post about student get together"/>
       </div>
     </div>
   );
