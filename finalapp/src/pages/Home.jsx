581773ec10a21413a785a4475fefdad73b2c9668
gabbyylu <Gabbylu890@gmail.com>
2026-03-20 09:47:02 -0700

    added quote component

--- finalapp/src/pages/Home.jsx
+++ finalapp/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import Heading from "../components/Heading";
 import ViewPost from "../components/ViewPost";
 import WomanPost from "../assets/WomanWorkingPost.png"
+import Quote from "../components/Quote";
 
 function Home() {
   return (
     <div>
     <Heading myTitle="WELCOME TO THE HOME PAGE"/>
+    <div className="quote flex justify-center mt-50">
+        <Quote randomQuote="Be yourself; everyone else is already taken" author= "Oscar Wilde" />
+    </div>
+
+    <br></br>
     <ViewPost imagePost={WomanPost}/>
     </div>
   );
