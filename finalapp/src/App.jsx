887c8becd441f3661474214c5133899ef3c3265b
“erik1D” <eraik.diaz7830@gmail.com>
2026-03-21 09:29:26 -0700

    Reverted

--- finalapp/src/App.jsx
+++ finalapp/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Profile from "./pages/Profile.jsx";
-import Login from "./pages/Login.jsx";
 
 function App() {
   return (
@@ -10,15 +9,13 @@ function App() {
       <nav className="text-center text-2xl m-3">
         <Link to="/">Home</Link> |{" "}
         <Link to="/about">About</Link> |{" "}
-        <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/login">Login</Link>
+        <Link to="/profile">Profile</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
