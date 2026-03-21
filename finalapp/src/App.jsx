c2733dd62a169eb4e5acd79a7363a53e39f2cae8
“erik1D” <eraik.diaz7830@gmail.com>
2026-03-21 09:16:38 -0700

    Added the Login Page

--- finalapp/src/App.jsx
+++ finalapp/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Profile from "./pages/Profile.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
@@ -9,13 +10,15 @@ function App() {
       <nav className="text-center text-2xl m-3">
         <Link to="/">Home</Link> |{" "}
         <Link to="/about">About</Link> |{" "}
-        <Link to="/profile">Profile</Link>
+        <Link to="/profile">Profile</Link> |{" "}
+        <Link to="/login">Login</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ finalapp/src/components/LoginBox.jsx
@@ -0,0 +1,27 @@
+function LoginBox(){
+    return(
+        <div className="bg-gray-300 border-2 w-xl h-150">
+            <div className="text-center mt-4">
+                <h1 className="text-5xl font-google-sans">Login</h1> 
+            </div>
+            <div className="m-20 justify-center-safe">
+                <form action="">
+                    <label For="emailBox" className="font-google-sans">Email</label>
+                    <input type="email" name="" id="emailBox" className="w-100 bg-gray-400 text-2xl outline-none"/>
+                    <br />
+                    <br />
+                    <label For="pwBox" className="font-google-sans">Password</label>
+                    <input type="password" name="" id="pwBox" className="w-100 bg-gray-400 text-2xl outline-none"/>
+                    <br />
+                    <br />
+                    <input type="checkbox" name="" id="myCheckBox" value="Yes"/>
+                    <label for="myCheckBox" className="ml-3 font-google-sans">Remember Me?</label>
+                    <br />
+                    <button type="submit" className="text-xl bg-white w-30 rounded-2xl ml-35 mt-5">Login</button>      
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default LoginBox
