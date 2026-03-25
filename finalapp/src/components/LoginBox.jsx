53eb80911007fb14abbba8d2c5416a69e47ead90
“erik1D” <eraik.diaz7830@gmail.com>
2026-03-25 11:49:46 -0700

    Adding Profile Page

--- finalapp/src/components/LoginBox.jsx
+++ finalapp/src/components/LoginBox.jsx
@@ -1,23 +1,23 @@
 function LoginBox(){
    return(
-       <div className="bg-gray-300 border-2 w-xl h-150">
+       <div className="bg-[#FFD666] rounded-4xl outline-3 outline-offset-3 w-xl h-150">
            <div className="text-center mt-4">
                <h1 className="text-5xl font-google-sans">Welcome Back!</h1>
            </div>
            <div className="m-20 justify-center-safe">
                <form action="">
                    <label For="emailBox" className="font-google-sans">Email</label>
-                   <input type="email" name="" id="emailBox" className="w-100 bg-gray-400 text-2xl outline-none"/>
+                   <input type="email" name="" id="emailBox" className="w-100 text-2xl border-b outline-none"/>
                    <br />
                    <br />
                    <label For="pwBox" className="font-google-sans">Password</label>
-                   <input type="password" name="" id="pwBox" className="w-100 bg-gray-400 text-2xl outline-none"/>
+                   <input type="password" name="" id="pwBox" className="w-100 text-2xl border-b outline-none"/>
                    <br />
                    <br />
-                   <input type="checkbox" name="" id="myCheckBox" value="Yes"/>
+                   <input type="checkbox" className="accent-black" name="" id="myCheckBox" value="Yes"/>
                    <label for="myCheckBox" className="ml-3 font-google-sans">Remember Me?</label>
                    <br />
-                   <button type="submit" className="text-xl bg-white w-30 rounded-2xl ml-35 mt-5">Login</button>     
+                   <button type="submit" className="text-xl bg-[#FFF8E5] w-30 rounded-xl ml-35 mt-5 transition duration-150 ease-in-out hover:scale-110">Login</button>     
                </form>
            </div>
        </div>
--- finalapp/src/components/ProfilePic.jsx
+++ finalapp/src/components/ProfilePic.jsx
@@ -3,7 +3,7 @@ import dog from '../assets/Dog.png'
 function ProfilePics(){
     return(
         <> 
-        <div className=" bg-gray-300 w-60 rounded-r-lg roudned-bl-lg border-r-4 border-b-4 border-dotted m-4">
+        <div className=" bg-[#FFF8E5] w-60 rounded-2xl m-4">
             <img src={dog} alt="" draggable="false"/>
             
         </div>
