adc7cc1371082172b7df6b1808d82b8c5139ca28
“erik1D” <eraik.diaz7830@gmail.com>
2026-04-29 11:14:30 -0700

    Refactor project structure by removing obsolete files and restoring essential ones

--- finalapp/vite.config.js
+++ finalapp/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'    // <-- ADD THIS
  
 export default defineConfig({
-  plugins: [react(), tailwindcss()],    
+  plugins:[react(), tailwindcss()],
   base: '/final-project-erik-ava-gabby/',
-  
 })
