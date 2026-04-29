d04e120146f9de1ee68005bad483907244d26bbe
“erik1D” <eraik.diaz7830@gmail.com>
2026-04-29 11:20:05 -0700

    Revert "Refactor project structure by removing obsolete files and restoring essential ones"
    This reverts commit adc7cc1371082172b7df6b1808d82b8c5139ca28.

--- finalapp/vite.config.js
+++ finalapp/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'    // <-- ADD THIS
  
 export default defineConfig({
-  plugins:[react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],    
   base: '/final-project-erik-ava-gabby/',
+  
 })
